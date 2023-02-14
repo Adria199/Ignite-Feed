04d8109c9ab2fcb4194cf4478166b708fbd25a6e
Adrian Guilherme <adrianguilherme133@outlook.com>
2023-02-14 13:56:28 -0300

    inicial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './global.css';
+
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
+
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post />
+        </main>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
