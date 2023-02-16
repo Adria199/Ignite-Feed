036a8508f381169641136d32c2bdd289f2c3e71d
Adrian Guilherme <adrianguilherme133@outlook.com>
2023-02-15 23:27:15 -0300

    post render fix, and added the structure of post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,1 +1,19 @@
-<h1>Hello world</h1>
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+      <article className={styles.post}>
+          <header>
+             <div className={styles.author}>
+                <img 
+                 className={styles.avatar} 
+                 src="https://github.com/Adria199.png"
+                />
+                <div className={styles.authorInfo}>
+                    <strong>Adrian Guilherme</strong>
+                </div>
+             </div>
+          </header>
+      </article>
+    )
+  }
