12e3f0c176bb8279b47b6a7f3755c9e19d5eb6b4
Adrian Guilherme <adrianguilherme133@outlook.com>
2023-02-20 16:14:58 -0300

    use state implemented for adding new comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,44 @@ import { Post } from './components/Post'
 
 import styles from './App.module.css'
 
+const posts = [
+  {
+    Id: 1,
+    
+    author: {
+      avatarUrl : 'https://github.com/Adria199.png',
+      name: 'Adrian Guilherme',
+      role: 'Web Developer'
+    },
+
+   content: [
+     {type: 'paragraph', content:'Hello guys 👋'},
+     {type: 'paragraph', content:'I have just uploaded a new project on my portifolio. It´s a project i did at NLW Return, a rocketseat event. The name of the project is DoctorCare 🚀'},
+     {type: 'Link', content:'jane.design/doctorcare'}
+   ],
+
+   publishedAt: new Date('2023-02-20 14:01:00')
+  },
+
+  {
+    Id: 2,
+    
+    author: {
+      avatarUrl : 'https://github.com/Adria199.png',
+      name: 'Adrian Guilherme',
+      role: 'Web Developer'
+    },
+
+   content: [
+     {type: 'paragraph', content:'Hello guys 👋'},
+     {type: 'paragraph', content:'I have just uploaded a new project on my portifolio. It´s a project i did at NLW Return, a rocketseat event. The name of the project is DoctorCare 🚀'},
+     {type: 'Link', content:'jane.design/doctorcare'}
+   ],
+
+   publishedAt: new Date('2023-02-20 14:30:00')
+  },
+]
+
 function App() {
 
   return (
@@ -16,9 +54,15 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                 author={post.author}
+                 content={post.content}
+                 publishedAt={post.publishedAt} 
+              />
+            )
+          })}
         </main>
       </div>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,36 +1,54 @@
+import {format, formatDistanceToNow} from 'date-fns'
+
 import styles from './Post.module.css';
+
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
-export function Post() {
+export function Post({author, publishedAt, content}) {
+   const [comments, setComments] = useState ([
+       1,
+       2,  
+   ])
+   
+    const publishedDateFormatted = format(publishedAt, "LLLL d 'at' HH:mm")
+
+    const publishedDateRelativeTonow = formatDistanceToNow(publishedAt, {
+      addSuffix: true
+    })
+
+    function handleCreateNewComment() {
+       event.preventDefault()
+
+       setComments([...comments, comments.lenght + 1])
+    }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src='https://github.com/Adria199.png' />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Adrian Guilherme</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="15 february 23:34h" dateTime="2023-02-15 23:34">Published one hour ago</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeTonow}</time>
       </header>
 
-
-
       <div className={styles.content}>
-        <p>Hello guys 👋</p>
-        <p>I have just uploaded a new project on my portifolio. It´s a project i did at NLW Return, a rocketseat event. The name of the project is DoctorCare 🚀</p>
-        <p><a href="">jane.design/doctorcare</a></p>
-        <p>
-          <a href=""> #novoprojeto </a> {' '}
-          <a href=""> #nlw </a> {' '}
-          <a href="">#rocketseat </a>
-        </p>
+        {content.map(line => {
+          if (line.type == 'paragraph') {
+            return <p>{line.content}</p>
+          } else if (line.type == 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Leave a feedback</strong>
 
         <textarea 
@@ -43,9 +61,9 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
 
     </article>
