8cb37ae3da532d0d5b57f5d09051366dc1a1320d
Adrian Guilherme <adrianguilherme133@outlook.com>
2023-02-20 13:21:55 -0300

    Added Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,13 @@
 import styles from './Post.module.css';
 import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/Adria199.png"
-          />
+          <Avatar src='https://github.com/Adria199.png' />
           <div className={styles.authorInfo}>
             <strong>Adrian Guilherme</strong>
             <span>Web Developer</span>
